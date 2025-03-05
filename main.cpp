cd9bf393d8526fd33f992acab1bd94600183acda
aiden cox <cox.378@wright.edu>
2025-03-05 14:26:07 -0500

    user input

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+    //cout << (insertion operator)
+    //cin >> (extraction operator)
 int main() {
+
+    std::string name;
+    int age;
+
+    std::cout << "Whats your full name?: ";
+    std::getline(std::cin >> std::ws, name); // will accept white space
+    //cin followed by getline() the \n is accepted into the buffer and will cause issues if we swapped the two quesiions
+    // to stop from happening, we use std::ws
+
+    std::cout << "Whats your age?: ";
+    std::cin >> age;
+
+    std::cout  << "Hello, " << name << '\n';
+    std::cout <<  "You are " << age << " years old";
 
 
     return 0;
